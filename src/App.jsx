d44a50ee106a2888244d00229db6df222a4046b4
WhiteCat6142 <whitecat6142+git@gmail.com>
2024-02-16 18:41:52 +0900

    writable relays

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 
 import { createEffect, createSignal, For } from "solid-js";
-import { createStore} from "solid-js/store";
+import { createStore } from "solid-js/store";
 
 import { createRxNostr, createRxForwardReq, createRxBackwardReq } from "rx-nostr";
 import { reduce } from 'rxjs';
 import { themeChange } from 'theme-change'
 
 
-function createLocalStore(initState){
+function createLocalStore(initState) {
   const [state, setState] = createStore(initState);
   if (localStorage.comments) setState(JSON.parse(localStorage.comments));
   //createEffect(() => (localStorage.comments = JSON.stringify(state)));
@@ -19,8 +19,38 @@ const [newTitle, setTitle] = createSignal("");
 
 const [comments, setComments] = createLocalStore([]);
 
-const App = () => {
+const Comm = (props) => {
+  const comments = props.c;
+  return (
+    <For each={comments}>
+      {(comment, _i) => (
+        <div class="chat chat-start md:text-lg">
+          <div class="chat-bubble whitespace-pre-wrap break-all">
+            {comment.content}
+          </div>
+          <div class="chat-footer">
+            <button
+              class="btn btn-sm"
+              onClick={() => {
+                rxNostr.send({
+                  kind: 7,
+                  content: "🤙",
+                  tags: [["e", comment.id], ["p", comment.pubkey]]
+                });
+              }
+              }
+            >
+              &#x1f919;
+            </button>
+            {new Date(comment.created_at * 1000).toLocaleTimeString('en-UK')}
+          </div>
+        </div>
+      )}
+    </For>
+  );
+};
 
+const App = () => {
   const addComment = (e) => {
     e.preventDefault();
     rxNostr.send({
@@ -50,58 +80,37 @@ const App = () => {
       <button class="btn" data-set-theme="" data-act-class="ACTIVECLASS">Reset</button>
       <button class="btn" data-set-theme="dark" data-act-class="ACTIVECLASS">Dark</button>
       <button class="btn" data-set-theme="light" data-act-class="ACTIVECLASS">Light</button>
-      <For each={comments}>
-        {(comment, _i) => (
-          <div
-            class="chat chat-start md:text-lg"
-          >
-            <div class="chat-bubble whitespace-pre-wrap break-all">
-              {comment.content}
-            </div>
-            <div class="chat-footer">
-            <button
-              class="btn btn-sm"
-              onClick={() => {
-                rxNostr.send({
-                  kind: 7,
-                  content: "🤙",
-                  tags:[["e",comment.id],["p",comment.pubkey]]
-                });
-              }
-              }
-            >
-              &#x1f919;
-            </button>
-            {new Date(comment.created_at*1000).toLocaleTimeString('en-UK')}
-            </div>
-          </div>
-        )}
-      </For>
+      <Comm c={comments}></Comm>
     </>
   );
 };
 
 const rxNostr = createRxNostr();
-rxNostr.setDefaultRelays(["wss://yabu.me"]);
+const relays = ["wss://nos.lol", "wss://relay-jp.nostr.wirednet.jp", "wss://nostr.holybea.com", "wss://nostr-relay.nokotaro.com"].map(url => { return { url, write: true } });
+rxNostr.setDefaultRelays([{
+  url: "wss://yabu.me",
+  read: true,
+  write: true
+}, ...relays]);
 
 const rxReq0 = createRxBackwardReq();
 
 rxNostr.use(rxReq0).pipe(reduce((list, packet) => {
   list.push(packet.message[2]);
   return list;
-}, [])).subscribe(list=>{
-  setComments([...comments,...list]);
+}, [])).subscribe(list => {
+  setComments([...comments, ...list]);
 });
 
-rxReq0.emit({ kinds: [1] , limit:100});
+rxReq0.emit({ kinds: [1], limit: 100 });
 rxReq0.over();
 
 const rxReq = createRxForwardReq();
 
 rxNostr.use(rxReq).subscribe((packet) => {
-  setComments([packet.message[2],...comments]);
+  setComments([packet.message[2], ...comments]);
 });
 
-rxReq.emit({ kinds: [1] , limit:0});
+rxReq.emit({ kinds: [1], limit: 0 });
 
 export default App
